b47dca705d480dfbe2dd1bb17a346c4e70421de0
ferdous kazemi <ferdouskazemi1000@gmail.com>
2023-08-12 15:40:42 +0430

    fix

--- constants/pages.tsx
+++ constants/pages.tsx
@@ -1,7 +1,7 @@
 export const DASHBOARD = '/';
-export const ZOOMABLE_SUNBURST = '/zoomable-sunburst';
-export const COOLAPSIBLE_TREE = '/collapsible-tree';
-export const STREAMGRAPH_TRANSITIONS = '/streamgraph-transitions';
-export const ARC_DIAGRAM = '/arc-diagram';
-export const ZOOMABLE_CIRCLE_PACKING = '/zoomable-circle-packing';
+export const ZOOMABLE_SUNBURST = '/chart/zoomable-sunburst';
+export const ZOOMABLE_CIRCLE_PACKING = '/chart/zoomable-circle-packing';
+export const COOLAPSIBLE_TREE = '/chart/collapsible-tree';
+export const STREAMGRAPH_TRANSITIONS = '/chart/streamgraph-transitions';
+export const ARC_DIAGRAM = '/chart/arc-diagram';
 
