d557cb262b79a70c7827313a204f11c8f2047bfb
ferdous kazemi <ferdouskazemi1000@gmail.com>
2023-08-12 14:51:26 +0430

    Adding Search box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,55 @@
 import Head from 'next/head';
 import PublicLayout from '../layout/public-layout';
-import { Col, Row } from 'antd';
+import { Col, Input, Row } from 'antd';
 import ReusableCard from '../components/custome-components/card-details';
+import { useState } from 'react';
+
+interface Card {
+  title: string;
+  imageUrl: string;
+  linkUrl: string;
+}
+
+const cards: Card[] = [
+  {
+    title: 'Zoomable Sunburst',
+    imageUrl: 'https://picsum.photos/200/300?grayscale',
+    linkUrl: '/zoomable-sunburst',
+  },
+  {
+    title: 'Coolapsible Tree',
+    imageUrl: 'https://picsum.photos/200/300?grayscale',
+    linkUrl: '/zoomable-circle-packing',
+  },
+  {
+    title: 'Streamgraph Transitions',
+    imageUrl: 'https://picsum.photos/200/300?grayscale',
+    linkUrl: '/zoomable-icicle',
+  },
+  {
+    title: 'Arc Diagram',
+    imageUrl: 'https://picsum.photos/200/300?grayscale',
+    linkUrl: '/zoomable-treemap',
+  },
+  {
+    title: 'Zoomable Partition',
+    imageUrl: 'https://picsum.photos/200/300?grayscale',
+    linkUrl: '/zoomable-partition',
+  },
+];
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredCards, setFilteredCards] = useState<Card[]>(cards);
+
+  const handleSearch = (query: string) => {
+    const filtered = cards.filter((card) =>
+      card.title.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredCards(filtered);
+    setSearchQuery(query);
+  };
+
   return (
     <PublicLayout title="Dashboard">
       <Head>
@@ -16,49 +62,28 @@ export default function Home() {
           Powerful, Beautiful, Easy Data Visiulation.{' '}
         </p>
       </div>
-      <Row gutter={[16,16]} className='mt-8'>
-        <Col xs={24} md={12} lg={6}>
-          <ReusableCard
-            title={'Zoomable Sunburst'}
-            imageUrl={'https://picsum.photos/200/300?grayscale'}
-            linkUrl={'asdasdsadasd'}
-          />
-        </Col>
-        <Col xs={24} md={12} lg={6}>
-          <ReusableCard
-            title={'Zoomable asd'}
-            imageUrl={'https://picsum.photos/200/300?grayscale'}
-            linkUrl={'asdasdsadasd'}
-          />
-        </Col>
-        <Col xs={24} md={12} lg={6}>
-          <ReusableCard
-            title={'Zoomable Sunbasdasurst'}
-            imageUrl={'https://picsum.photos/200/300?grayscale'}
-            linkUrl={'asdasdsadasd'}
-          />
-        </Col>
-        <Col xs={24} md={12} lg={6}>
-          <ReusableCard
-            title={'Zoomable eqweqwe'}
-            imageUrl={'https://picsum.photos/200/300?grayscale'}
-            linkUrl={'asdasdsadasd'}
-          />
-        </Col>
-        <Col xs={24} md={12} lg={6}>
-          <ReusableCard
-            title={'Zoomable qwersd'}
-            imageUrl={'https://picsum.photos/200/300?grayscale'}
-            linkUrl={'asdasdsadasd'}
-          />
-        </Col>
-        <Col xs={24} md={12} lg={6}>
-          <ReusableCard
-            title={'Zoomable Sqweqweunburst'}
-            imageUrl={'https://picsum.photos/200/300?grayscale'}
-            linkUrl={'asdasdsadasd'}
-          />
-        </Col>
+      <div className="container mx-auto my-8 flex justify-center">
+        <Input
+          placeholder="Search..."
+          value={searchQuery}
+          className="border rounded-md py-2 px-4 shadow-sm focus:ring-violet-800 focus:border-violet-800 max-w-[500px]"
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
+      <Row gutter={[16, 16]}>
+        {filteredCards.length > 0 ? (
+          filteredCards.map((card, index) => (
+            <Col key={index} xs={24} md={12} lg={6}>
+              <ReusableCard
+                title={card.title}
+                imageUrl={card.imageUrl}
+                linkUrl={card.linkUrl}
+              />
+            </Col>
+          ))
+        ) : (
+          <p className="text-center text-xl w-full ">No results found.</p>
+        )}
       </Row>
     </PublicLayout>
   );
