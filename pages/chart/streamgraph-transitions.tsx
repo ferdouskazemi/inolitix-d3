28a4b92182f256f2116b954bfde8c7cecf397924
ferdous kazemi <ferdouskazemi1000@gmail.com>
2023-08-12 15:41:11 +0430

    fix

---
+++ pages/chart/streamgraph-transitions.tsx
@@ -0,0 +1,12 @@
+import PublicLayout from "../../layout/public-layout";
+import StreamgraphTransitionsChart  from "../../components/streamgraph-transitions";
+const StreamgraphTransitions = () => {
+    return ( 
+        <PublicLayout title="Zoomable Suburst Chart">
+            <p className="text-2xl font-semibold text-center">Streamgraph Transitions chart</p>
+            <StreamgraphTransitionsChart  />
+        </PublicLayout>
+     );
+}
+ 
+export default StreamgraphTransitions;
