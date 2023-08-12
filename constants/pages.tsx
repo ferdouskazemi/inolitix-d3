708a04b3b145547d0f120f43cb46e63679a50c34
ferdous kazemi <ferdouskazemi1000@gmail.com>
2023-08-12 15:09:46 +0430

    adding menu

--- constants/pages.tsx
+++ constants/pages.tsx
@@ -3,3 +3,5 @@ export const ZOOMABLE_SUNBURST = '/zoomable-sunburst';
 export const COOLAPSIBLE_TREE = '/collapsible-tree';
 export const STREAMGRAPH_TRANSITIONS = '/streamgraph-transitions';
 export const ARC_DIAGRAM = '/arc-diagram';
+export const ZOOMABLE_CIRCLE_PACKING = '/zoomable-circle-packing';
+
