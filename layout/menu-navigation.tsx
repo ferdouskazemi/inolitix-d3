bdd656b1bb68b4cb482d1d36e452e09efc8096d7
ferdous kazemi <ferdouskazemi1000@gmail.com>
2023-08-12 20:44:16 +0430

    add some things new

--- layout/menu-navigation.tsx
+++ layout/menu-navigation.tsx
@@ -5,14 +5,17 @@ import {
   COOLAPSIBLE_TREE,
   DASHBOARD,
   STREAMGRAPH_TRANSITIONS,
+  TREEMAP,
+  ZOOMABLE_BAR,
   ZOOMABLE_CIRCLE_PACKING,
   ZOOMABLE_SUNBURST,
 } from '../constants/pages';
 
 import { FcComboChart } from 'react-icons/fc';
+import {AiFillHome} from 'react-icons/ai';
 
 const menuItems = [
-  { key: '1', label: 'Dashboard', route: DASHBOARD },
+  { key: '1', label: 'Dashboard', icon: <AiFillHome />, route: DASHBOARD },
   {
     key: '2',
     label: 'Zoomable Sunburst',
@@ -42,7 +45,19 @@ const menuItems = [
     label: 'Zoomable circle packing',
     icon: <FcComboChart />,
     route: ZOOMABLE_CIRCLE_PACKING,
-  }
+  },
+  {
+    key: '7',
+    label: 'Treemap',
+    icon: <FcComboChart />,
+    route: TREEMAP,
+  },
+  {
+    key: '8',
+    label: 'Zoomable Bar Chart',
+    icon: <FcComboChart />,
+    route: ZOOMABLE_BAR,
+  },
 ];
 
 const MenuNavigation = ({ currentRoute }: { currentRoute: string }) => {
