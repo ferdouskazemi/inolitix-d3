8f8f86264ea5f4ab83c29ded23c1b26d25ce35e4
ferdouskazemi350 <ferdous@truetale.io>
2023-08-27 16:18:02 +0430

    fix

--- pages/chart/zoomable-circle-packing.tsx
+++ pages/chart/zoomable-circle-packing.tsx
@@ -13,7 +13,7 @@ const ZoomableCirclePacking = () => {
   const handleFileInputChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    handleFileChange(event, setJsonData); // Call the imported handleFileChange function
+    handleFileChange(event, setJsonData);
   };
 
   return (
